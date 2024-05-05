0f57795ba3a16aa8acb00c20fd86b7780d1e1f09
Nurullah Coşkun <97886064+AsNott@users.noreply.github.com>
2024-05-05 11:00:01 +0300

    Combobox Created Combobox get attributes

--- Basic_Projects2000/Currency_Converter2000/currency_converter.py
+++ Basic_Projects2000/Currency_Converter2000/currency_converter.py
@@ -1,6 +1,17 @@
 import tkinter as tk
+from tkinter import ttk
 from bs4 import BeautifulSoup
 import requests
+
+#Functions
+def _currency():
+    if cbox.get() == 'Usd':
+        label.config(text = 'Dollar Choosed')
+    elif cbox.get() == 'Euro':
+        label.config(text='Euro Choosed')
+    elif cbox.get() == 'Sterlin':
+        label.config(text='Sterlin Choosed')
+
 
 #api data extraction
 url_usd = "https://www.google.com/finance/quote/USD-TRY?hl=tr"
@@ -29,7 +40,23 @@ casted_sterlin = float(sterlin.replace(",", "."))
 #ui
 ui = tk.Tk()
 ui.title("Currency Converter 2000")
+ui.geometry("475x320+600+250")
+ui.resizable(False, False)
 
+_input = tk.Entry(ui, width=25, font="Times 12 bold")
+_input.place(x=150, y=110)
 
+label = tk.Label(ui, text="Price : ", font="Times 17")
+label.place(x=40, y=105)
+label2 = tk.Label(ui, text="Result : ", font="Times 17")
+label2.place(x=150, y=170)
 
+n = tk.StringVar()
+cbox = ttk.Combobox(ui, width=27, textvariable=n, font="Times 15")
+cbox['values'] = ('Usd', 'Euro', 'Sterlin')
+cbox.place(x=75, y=40)
+cbox.current(0)
+
+currency = tk.Button(ui, text="Currency", font="Times 17", command=_currency)
+currency.place(x=75, y=250)
 ui.mainloop()
