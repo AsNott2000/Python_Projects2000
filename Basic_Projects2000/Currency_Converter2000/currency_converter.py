4b8d2bf914bfb78a4c4498effdfaf71611f6f83b
Nurullah Coşkun <97886064+AsNott@users.noreply.github.com>
2024-05-07 10:59:01 +0300

    new combobox

--- Basic_Projects2000/Currency_Converter2000/currency_converter.py
+++ Basic_Projects2000/Currency_Converter2000/currency_converter.py
@@ -6,17 +6,25 @@ import requests
 #Functions
 def _currency():
     _entry = float(_input.get())
-    if cbox.get() == 'Usd':
+    if cbox_input.get() == 'Usd':
         curency_dollar = _entry*casted_dollar
         label_result.config(text = round(curency_dollar, 2))
-    elif cbox.get() == 'Euro':
+    elif cbox_input.get() == 'Euro':
         curency_euro = _entry * casted_euro
         label_result.config(text = round(curency_euro, 2))
-    elif cbox.get() == 'Sterlin':
+    elif cbox_input.get() == 'Sterlin':
         curency_sterlin = _entry * casted_sterlin
         label_result.config(text=round(curency_sterlin, 2))
 
-
+def _outputChanged():
+    if cbox_output.get == 'TL':
+        label_output.config(text = "TL")
+    elif cbox_output.get == 'Usd':
+        label_output.config(text="Usd")
+    elif cbox_output.get == 'Euro':
+        label_output.config(text="Euro")
+    elif cbox_output.get == 'Sterlin':
+        label_output.config(text="Sterlin")
 #api data extraction
 url_usd = "https://www.google.com/finance/quote/USD-TRY?hl=tr"
 url_euro = "https://www.google.com/finance/quote/EUR-TRY?hl=tr"
@@ -53,15 +61,23 @@ label = tk.Label(ui, text="Price : ", font="Times 17")
 label.place(x=50, y=105)
 label_result = tk.Label(ui, text="", font="Times 17")
 label_result.place(x=150, y=170)
-label_tl = tk.Label(ui, text="Result : ", font="Times 17")
-label_tl.place(x=50, y=170)
+label_output = tk.Label(ui, text="Result : ", font="Times 17")
+label_output.place(x=50, y=170)
 
 n = tk.StringVar()
-cbox = ttk.Combobox(ui, width=27, textvariable=n, font="Times 15")
-cbox['values'] = ('Usd', 'Euro', 'Sterlin')
-cbox.place(x=70, y=40)
-cbox.current(0)
+cbox_input = ttk.Combobox(ui, width=10, textvariable=n, font="Times 15")
+cbox_input['values'] = ('TL','Usd', 'Euro', 'Sterlin')
+cbox_input.place(x=70, y=40)
+cbox_input.current(1)
+
+m = tk.StringVar()
+cbox_output = ttk.Combobox(ui, width=10, textvariable=m, font="Times 15")
+cbox_output['values'] = ('TL','Usd', 'Euro', 'Sterlin')
+cbox_output.place(x=220, y=40)
+cbox_output.current(0)
 
 currency = tk.Button(ui, text="Currency", font="Times 17", command=_currency)
 currency.place(x=75, y=250)
+
+_outputChanged()
 ui.mainloop()
