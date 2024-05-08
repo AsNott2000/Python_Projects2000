2606dc45f78d6f4829ee48ac6239e1edb89001af
Nurullah Coşkun <97886064+AsNott@users.noreply.github.com>
2024-05-09 00:14:31 +0300

    Completed

--- Basic_Projects2000/NumberGuessingGame2000/guessing2000.py
+++ Basic_Projects2000/NumberGuessingGame2000/guessing2000.py
@@ -1,23 +1,25 @@
 import random
 x = 0
 while x == 0:
+    print("Information: Keep a count in your mind. I will try to guess. If the number I guess is large, write it DOWN, "
+          "if it is small, write it UP. ıf correct, write CORRECT.")
     print("input the number (pls 0 and 100 between): ")
     num = int(input())
     if num > 0 and num <100:
         x = x+1
 y = 0
-RandomNumberYukari = 1
-RandomNumberAsagi = 100
+RandomNumberUp = 1
+RandomNumberDown = 100
 while y == 0:
-    random_number = random.randint(RandomNumberYukari,RandomNumberAsagi)
+    random_number = random.randint(RandomNumberUp,RandomNumberDown)
     print(random_number)
-    print("asagi mi yukari mi dogru mu")
-    asagi_yukari_dogru = input()
-    if asagi_yukari_dogru == "dogru":
-        print("dogru buldum")
-        y=1
-    elif asagi_yukari_dogru == "yukari":
-        RandomNumberYukari = random_number + 1
-    elif asagi_yukari_dogru == "asagi":
-        RandomNumberAsagi = random_number - 1
+    print("up, down or correct?")
+    up_down_correct = input()
+    if up_down_correct == "correct":
+        print("I founded!")
+        y = 1
+    elif up_down_correct == "up":
+        RandomNumberUp = random_number + 1
+    elif up_down_correct == "down":
+        RandomNumberDown = random_number - 1
 
