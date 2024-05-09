c35ae5094e1ea1c41499ca44ed2b6095e65d86be
Nurullah Coşkun <97886064+AsNott@users.noreply.github.com>
2024-05-09 16:06:11 +0300

    Completed hangman

---
+++ Basic_Projects2000/005_RockPaperScissors3000/RockPaperScissors3000.py
@@ -0,0 +1,18 @@
+import random
+import tkinter as tk
+import time
+
+
+
+
+print("Hello my Friendoo! Watashi wa Kira Yoshikage")
+time.sleep(2)
+print("I'm 33 years old. My house is in the northeast section of Morioh, where all the villas are, and I am not married.")
+time.sleep(2)
+print("I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest.")
+time.sleep(2)
+print("Now we gonna play ROCK, PAPER, SCISSORS!")
+time.sleep(2)
+print("Be careful because you have 3 attempts. If you wanna win game, you defeat me 3 times!")
+time.sleep(2)
+print("Lets Play!!")
