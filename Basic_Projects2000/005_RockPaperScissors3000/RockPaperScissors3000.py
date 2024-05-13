f1709730136152db396d1030ff0a11c90f22157a
Nurullah Coşkun <97886064+AsNott@users.noreply.github.com>
2024-05-13 23:04:31 +0300

    progressing

--- Basic_Projects2000/005_RockPaperScissors3000/RockPaperScissors3000.py
+++ Basic_Projects2000/005_RockPaperScissors3000/RockPaperScissors3000.py
@@ -1,63 +1,97 @@
-import random
 import tkinter as tk
-import time
+from PIL.ImageTk import PhotoImage
+from PIL import Image
+import random
+from tkinter import messagebox
 
-def theGame():
+fence = tk.Tk()
+fence.geometry("518x300")
+fence.resizable(width= False, height= False)
 
-
-    score_kira = 0
-    score_user = 0
-    print("Kira's Score: ", score_kira)
-    print("Your Score: ", score_user)
-    print("_____________")
-    while score_user < 3 and score_kira < 3:
-        moves = [
+moves = [
             "rock",
             "paper",
             "scissors"
         ]
-        moveKira = random.choice(moves)
-        moveInput = input("rock, paper or scissor? ").lower()
-        print("Kira's Choices: ", moveKira)
-        if moveInput == moveKira:
-            print("tie")
-        elif moveInput == "rock":
-            if moveKira == "paper":
-                print("Kira Won")
-                score_kira += 1
-            elif moveKira == "scissors":
-                print("You Won")
-                score_user += 1
-        elif moveInput == "paper":
-            if moveKira == "scissors":
-                print("Kira Won")
-                score_kira += 1
-            elif moveKira == "rock":
-                print("You Won")
-                score_user += 1
-        elif moveInput == "scissors":
-            if moveKira == "rock":
-                print("Kira Won")
-                score_kira += 1
-            elif moveKira == "paper":
-                print("You Won")
-                score_user += 1
+kira_count = 0
+user_count = 0
+def _rock():
+    moveKira = random.choice(moves)
+    if moveKira == "rock":
+        messagebox.showinfo("INFO", "TIE"+"\n\nKira's Move= "+ moveKira)
+    elif moveKira == "paper":
+        messagebox.showinfo("INFO", "KIRA WON"+"\n\nKira's Move= "+ moveKira)
+    else:
+        messagebox.showinfo("INFO", "YOU WON"+"\n\nKira's Move= "+ moveKira)
+def _paper():
+    moveKira = random.choice(moves)
+    if moveKira == "rock":
+        messagebox.showinfo("INFO", "YOU WIN"+"\n\nKira's Move= "+ moveKira)
+    elif moveKira == "paper":
+        messagebox.showinfo("INFO", "TIE"+"\n\nKira's Move= "+ moveKira)
+    else:
+        messagebox.showinfo("INFO", "KIRA WIN"+"\n\nKira's Move= "+ moveKira)
+def _scissors():
+    moveKira = random.choice(moves)
+    if moveKira == "rock":
+        messagebox.showinfo("INFO", "KIRA WON"+"\n\nKira's Move= "+ moveKira)
+    elif moveKira == "paper":
+        messagebox.showinfo("INFO", "YOU WON"+"\n\nKira's Move= "+ moveKira)
+    else:
+        messagebox.showinfo("INFO", "TIE"+"\n\nKira's Move= "+ moveKira)
+
+#Rock Button
+rock_img = Image.open('./img/rock.png')
+RockWidth, RockHeight = rock_img.size
+
+rock_w = int(RockWidth/3)
+rock_h = int(RockHeight/3)
+
+sizedRock = rock_img.resize((rock_w,rock_h))
+
+Rock_Pimage = PhotoImage(sizedRock)
+
+button_Rock = tk.Button(fence, image=Rock_Pimage, command= _rock)
+button_Rock.place(x=0,y=0)
+
+#Paper Button
+paper_img = Image.open('./img/paper.png')
+PaperWidth, PaperHeight = paper_img.size
+
+paper_w = int(PaperWidth/3)
+paper_h = int(PaperHeight/3)
+
+sizedPaper = paper_img.resize((paper_w,paper_h))
+
+Paper_Pimage = PhotoImage(sizedPaper)
+
+button_Paper = tk.Button(fence, image=Paper_Pimage, command=_paper)
+button_Paper.place(x=175,y=0)
+
+#Scissors Button
+scissors_img = Image.open('./img/scissors.png')
+ScissorsWidth, ScissorsHeight = scissors_img.size
+
+scissors_w = int(ScissorsWidth/3)
+scissors_h = int(ScissorsHeight/3)
+
+sizedScissors = scissors_img.resize((scissors_w,scissors_h))
+
+Scissors_Pimage = PhotoImage(sizedScissors)
+
+button_Scissors = tk.Button(fence, image=Scissors_Pimage, command=_scissors)
+button_Scissors.place(x=350,y=0)
+
+#score Table
+label_Kira = tk.Label(text="Kira's Score:", font=("Montserrat", 25))
+label_Kira.place(x=15, y=200)
+label_User = tk.Label(text="User's Score:", font=("Montserrat", 25))
+label_User.place(x=15, y=240)
+
+Kira_Score = tk.Label(text="-", font=("Montserrat", 25))
+Kira_Score.place(x=235, y=200)
+User_Score = tk.Label(text="-", font=("Montserrat", 25))
+User_Score.place(x=235, y=240)
 
 
-        print("Kira's Score: ", score_kira)
-        print("Your Score: ", score_user)
-        print("_____________")
-
-print("Hello my Friendoo! Watashi wa Kira Yoshikage")
-time.sleep(1)
-print("I'm 33 years old. My house is in the northeast section of Morioh, where all the villas are, and I am not married.")
-time.sleep(1)
-print("I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest.")
-time.sleep(1)
-print("Now we gonna play ROCK, PAPER, SCISSORS!")
-time.sleep(1)
-print("Be careful because you have 3 attempts. If you wanna win game, you defeat me 3 times!")
-time.sleep(1)
-print("Lets Play!!")
-
-theGame()
+tk.mainloop()
---
+++ Basic_Projects2000/005_RockPaperScissors3000/RockPaperScissors30001.py
@@ -0,0 +1,61 @@
+import random
+import tkinter as tk
+import time
+
+def theGame():
+    score_kira = 0
+    score_user = 0
+    print("Kira's Score: ", score_kira)
+    print("Your Score: ", score_user)
+    print("_____________")
+    while score_user < 3 and score_kira < 3:
+        moves = [
+            "rock",
+            "paper",
+            "scissors"
+        ]
+        moveKira = random.choice(moves)
+        moveInput = input("rock, paper or scissor? ").lower()
+        print("Kira's Choices: ", moveKira)
+        if moveInput == moveKira:
+            print("tie")
+        elif moveInput == "rock":
+            if moveKira == "paper":
+                print("Kira Won")
+                score_kira += 1
+            elif moveKira == "scissors":
+                print("You Won")
+                score_user += 1
+        elif moveInput == "paper":
+            if moveKira == "scissors":
+                print("Kira Won")
+                score_kira += 1
+            elif moveKira == "rock":
+                print("You Won")
+                score_user += 1
+        elif moveInput == "scissors":
+            if moveKira == "rock":
+                print("Kira Won")
+                score_kira += 1
+            elif moveKira == "paper":
+                print("You Won")
+                score_user += 1
+
+
+        print("Kira's Score: ", score_kira)
+        print("Your Score: ", score_user)
+        print("_____________")
+
+print("Hello my Friendoo! Watashi wa Kira Yoshikage")
+time.sleep(1)
+print("I'm 33 years old. My house is in the northeast section of Morioh, where all the villas are, and I am not married.")
+time.sleep(1)
+print("I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest.")
+time.sleep(1)
+print("Now we gonna play ROCK, PAPER, SCISSORS!")
+time.sleep(1)
+print("Be careful because you have 3 attempts. If you wanna win game, you defeat me 3 times!")
+time.sleep(1)
+print("Lets Play!!")
+
+theGame()
